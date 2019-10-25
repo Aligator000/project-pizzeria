a168605d93cac60ad63585b808d697389c19aae4
Justyna Idzi <justyna.idzi@gmail.com>
2019-10-25 18:54:40 +0200

    Subpage - booking

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,9 @@
+
 import {Product} from './components/Product.js';
 import {Cart} from './components/Cart.js';
 import {select, settings} from './settings.js';
 import {classNames, templates} from './settings.js';
-
+import {Booking} from './components/Booking.js';
 
 const app = {
   initMenu: function(){
@@ -11,6 +12,20 @@ const app = {
     for(let productData in thisApp.data.products){
       new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
     }
+  },
+
+  init: function(){
+    const thisApp = this;
+    console.log('*** App starting ***');
+    console.log('thisApp:', thisApp);
+    console.log('classNames:', classNames);
+    console.log('settings:', settings);
+    console.log('templates:', templates);
+    thisApp.initPages();
+    thisApp.initData();
+    thisApp.initCart();
+    console.log(thisApp.pages);
+    thisApp.initBooking();
   },
 
   initData: function(){
@@ -34,28 +49,74 @@ const app = {
 
   initCart: function(){
     const thisApp = this;
+    const cartElem = document.querySelector(select.containerOf.cart);
+    thisApp.cart = new Cart(cartElem);
 
     thisApp.productList = document.querySelector(select.containerOf.menu);
 
     thisApp.productList.addEventListener('add-to-cart', function(event){
       app.cart.add(event.detail.product);
     });
-
-    const cartElem = document.querySelector(select.containerOf.cart);
-    thisApp.cart = new Cart(cartElem);
   },
 
-  init: function(){
+  initPages: function(){
     const thisApp = this;
-    console.log('*** App starting ***');
-    console.log('thisApp:', thisApp);
-    console.log('classNames:', classNames);
-    console.log('settings:', settings);
-    console.log('templates:', templates);
-    thisApp.initData();
-    thisApp.initCart();
+    thisApp.pages = Array.from(document.querySelector(select.containerOf.pages).children);
+    console.log(thisApp.pages);
+    thisApp.navLinks = Array.from(document.querySelectorAll(select.nav.links));
+    console.log(thisApp.navLinks);
+
+
+    let pagesMatchingHash = [];
+    if(window.location.hash.length > 2){
+      const idFromHash = window.location.hash.replace('#/', '');
+
+      pagesMatchingHash = thisApp.pages.filter(function(page){
+        return page.id == idFromHash;
+      });
+      thisApp.activatePage(pagesMatchingHash.length ? pagesMatchingHash[0].id : thisApp.pages[0].id);
+    }
+
+    //thisApp.activatePage(thisApp.pages[0].id);
+    //console.log('wywolalo sie?', thisApp.pages[0].id);
+
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+        //get page id from href
+        const pageHref = clickedElement.getAttribute('href');
+        //console.log('href ',pageHref);
+        const pId = pageHref.replace('#', '');
+        console.log('pId', pId);
+        //activate page
+        thisApp.activatePage(pId);
+      });
+    }
   },
+
+  activatePage: function(pageId){
+    const thisApp = this;
+
+    for(let link of thisApp.navLinks){
+      link.classList.toggle(classNames.nav.active, link.getAttribute('href') == '#' + pageId);
+      console.log(link);
+    }
+    for(let page of thisApp.pages){
+      page.classList.toggle(classNames.pages.active, page.getAttribute('id') == pageId);
+      console.log(page);
+    }
+    window.location.hash = '#/' + pageId;
+  },
+
+  initBooking: function(){
+    const thisApp = this;
+    const bookingWidget = document.querySelector(select.containerOf.booking);
+    thisApp.booking = new Booking(bookingWidget);
+  },
+
 };
 app.init();
+
 
 
